9510af1a1fe0fc3150dc3b5296f27ba5c15df3da
JSF <javiersanchezferri12@gmail.com>
2024-09-24 12:01:35 +0200

    script player y distribución

---
+++ PhantomRush/Assets/PhantomRush_Root/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Private References")]
+    PlayerInput playerInput;
+
+    [Header("Public References")]
+
+    [Header("Input")]
+
+    [Header("Player Stats")]
+    [SerializeField] float speed;
+
+    //[Header("Combat Parameters")]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveUp(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void MoveDown(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void Special(InputAction.CallbackContext context)
+    {
+        
+    }
+}
